7edbba48f270cf7bff51aa00d1d4bede712b409b
Matt Dyson <matt@thedysons.net>
2025-06-05 21:38:13 +0000

    Fix incorrect environment variable

--- bot_webhook.py
+++ bot_webhook.py
@@ -13,7 +13,7 @@ DISCORD_WEBHOOK_URL = os.getenv("DISCORD_WEBHOOK_URL")
 MQTT_BROKER = os.getenv("MQTT_BROKER", "localhost")
 MQTT_PORT = int(os.getenv("MQTT_PORT", 1883))
 TIMEZONE = os.getenv("TIMEZONE", "UTC")
-WATCH_CHANNELS = [int(ch) for ch in os.getenv("WATCH_CHANNELS", "").split(",") if ch.strip().isdigit()]
+CHANNELS = [int(ch) for ch in os.getenv("CHANNELS", "").split(",") if ch.strip().isdigit()]
 
 if not DISCORD_WEBHOOK_URL:
     logging.error("DISCORD_WEBHOOK_URL not set in .env file.")
@@ -24,14 +24,14 @@ logging.info(f"Logging: {LOGLEVEL}")
 logging.info(f"Webhook: {DISCORD_WEBHOOK_URL}")
 logging.info(f"MQTT Broker: {MQTT_BROKER}:{MQTT_PORT}")
 logging.info(f"Timezone: {TIMEZONE}")
-logging.info(f"Watching channels: {WATCH_CHANNELS if WATCH_CHANNELS else 'All'}")
+logging.info(f"Watching channels: {CHANNELS if CHANNELS else 'All'}")
 
 setup_mqtt(
     webhook_url=DISCORD_WEBHOOK_URL,
     broker=MQTT_BROKER,
     port=MQTT_PORT,
     timezone=TIMEZONE,
-    watch_channels=WATCH_CHANNELS
+    watch_channels=CHANNELS
 )
 
 # Keep the container alive
